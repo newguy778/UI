a3d83a1d0ef92173f987f93f5391933d96a92f7c
= <=>
2022-06-30 20:19:12 -0500

    renamed main to routes, testing in req

---
+++ routes.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from person_data import *
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+async def Hg():
+    return {"asdasd": "Hello World"}
+
+
+@app.get("/fake/firstname")
+async def get_fake() -> str:
+    return Person_US_API().get_first_name()
+
+
+@app.get("/fake/lastname")
+async def get_fake_lastname() -> str:
+    return Person_US_API().get_last_name()
+
+if __name__ == ("__main__"):
+    uvicorn.run('main:app', reload=True, debug=True)
--- temp_req.py
+++ temp_req.py
@@ -1,19 +1,29 @@
+from time import sleep
 import requests
-import time
+# import time
 
-URL = "http://127.0.0.1:8000"
-
-
-def get_data():
-    resp = requests.get(f"{URL}/fake")
-    # resp.close()
-    return resp.text
+# URL = "http://127.0.0.1:8000"
 
 
-start = time.perf_counter()
-for i in range(500):
-    print(get_data())
-end = time.perf_counter()
+# def get_data():
+#     resp = requests.get(f"{URL}/fake")
+#     # resp.close()
+#     return resp.text
 
 
-print(f"Total Time = {(end - start)}")
+# start = time.perf_counter()
+# for i in range(500):
+#     print(get_data())
+# end = time.perf_counter()
+
+
+# print(f"Total Time = {(end - start)}")
+
+user_agent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36"
+
+for i in range(20):
+    URL = "https://github.com/kp9821"
+    resp = requests.get(URL, headers={'user-agent': user_agent})
+    print(resp.status_code)
+    resp.close()
+    sleep(2)
