a677f69b8d081f562148df39df58225a27c36052
= <=>
2022-08-02 23:13:58 -0700

    Pytest upgraded

--- fakeit/__init__.py
+++ fakeit/__init__.py
@@ -3,3 +3,6 @@ import sys
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
 __version__ = "0.1.0"
+
+
+__author__ = "KP"
--- tests/test_fakeit.py
+++ tests/test_fakeit.py
@@ -1,5 +1,8 @@
-from fakeit import __version__
+from fakeit import __version__, __author__
 
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_author():
+    assert __author__ == "KP"
