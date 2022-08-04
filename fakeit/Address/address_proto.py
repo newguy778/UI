ceee78454df3901fc78482ca8d539c3b1aeecf2c
KP <10536220+kp9821@users.noreply.github.com>
2022-08-03 20:16:17 -0500

    Working on Address

--- fakeit/Address/address_proto.py
+++ fakeit/Address/address_proto.py
@@ -1,18 +1,26 @@
 from faker.providers.address import Provider as AddressProvider
+
 # from Person.person_proto import in_generator
 from typing import Any
-from faker import Generator,Factory,Faker
+from faker import Generator, Factory, Faker
+
 
 class _AddressProto:
-
-    def __init__(self,local) -> None:
+    def __init__(self, local) -> None:
         self.locality = Faker(local)
-    
-    
-    def get_address(self):
-        return self.locality.current_country()
-        
 
-gh = _AddressProto('jp_JP')
+    def get_address(self):
+        return self.locality.address()
+
+
+gh = _AddressProto("en_IN")
 
 print(gh.get_address())
+
+# fk = Factory.create("en_IN")
+# fku = Factory.create("en_US")
+
+# # print(fk.address())
+# print(fk.address())
+# # print(fk.street_address())
+# print(fku.current_country())
