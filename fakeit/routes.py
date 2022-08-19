5663c552fef90651e776d76f8de06bd66a139957
KP <10536220+kp9821@users.noreply.github.com>
2022-08-19 18:43:15 -0500

    Allow CORS

--- fakeit/routes.py
+++ fakeit/routes.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import person_router, address_router
 import uvicorn
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(CORSMiddleware, allow_origins=origins)
 app.include_router(person_router.person_router)
 app.include_router(address_router.address_router)
 
